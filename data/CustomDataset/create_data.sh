23a0243bb794877ce88df39c0c4435c6d17c78c3
tolotrasamuel <rsamuel15@alustudent.com>
2018-09-16 21:55:33 +0400

    secon

--- data/CustomDataset/create_data.sh
+++ data/CustomDataset/create_data.sh
@@ -4,9 +4,9 @@ root_dir=$cur_dir/../..
 cd $root_dir
 
 redo=1
-data_root_dir="$HOME/MyDataset/CustomDataset"
-dataset_name="CustomDataset"
-mapfile="$root_dir/data/$dataset_name/labelmap.prototxt"
+dataset_name="tutorial_person"
+data_root_dir="$HOME/MyDataset/$dataset_name"
+mapfile="$data_root_dir/labelmap.prototxt"
 anno_type="detection"
 db="lmdb"
 min_dim=0
@@ -21,5 +21,5 @@ then
 fi
 for subset in test trainval
 do
-  python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/data/$dataset_name/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
+  python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $data_root_dir/Structure/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
 done
